1b510b825ac6e578b73ae242baac353f5cfd5a77
aislanlima13 <aislan.lima04@gmail.com>
2022-05-13 17:41:33 -0300

    Configura filtro de tarefas

--- app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
@@ -10,7 +10,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PriorityRepository(context: Context) : BaseRepository(context) {
+class PriorityRepository(val context: Context) : BaseRepository(context) {
 
     private val mRemote = RetrofitClient.createService(PriorityService::class.java)
     private val mDatabase = TaskDatabase.getDatabase(context).priorityDao()
--- app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.listener.ValidationListener
 import com.example.tasks.service.model.TaskModel
@@ -13,6 +14,7 @@ import com.example.tasks.service.repository.TaskRepository
 class AllTasksViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mTaskRepository = TaskRepository(application)
+    private var mTaskFilter = 0
 
     private val mValidation = MutableLiveData<ValidationListener>()
     var validation: LiveData<ValidationListener> = mValidation
@@ -20,8 +22,10 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
     private val mTaskList = MutableLiveData<List<TaskModel>>()
     var tasks: LiveData<List<TaskModel>> = mTaskList
 
-    fun list() {
-        mTaskRepository.all(object : APIListener<List<TaskModel>>{
+    fun list(taskFilter: Int) {
+        mTaskFilter = taskFilter
+
+        val listener = object : APIListener<List<TaskModel>> {
             override fun onSuccess(model: List<TaskModel>) {
                 mTaskList.value = model
             }
@@ -30,8 +34,20 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
                 mTaskList.value = arrayListOf()
                 mValidation.value = ValidationListener(str)
             }
+        }
 
-        })
+        when (mTaskFilter) {
+            TaskConstants.FILTER.ALL -> {
+                mTaskRepository.all(listener)
+            }
+            TaskConstants.FILTER.NEXT -> {
+                mTaskRepository.nextWeek(listener)
+            }
+            else -> {
+                mTaskRepository.overDue(listener)
+            }
+        }
+
     }
 
     fun complete(id: Int) {
@@ -45,7 +61,7 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
     private fun updateStatus(id: Int, complete: Boolean) {
         mTaskRepository.updateStatus(id, complete, object : APIListener<Boolean>{
             override fun onSuccess(model: Boolean) {
-                list()
+                list(mTaskFilter)
             }
 
             override fun onFailure(str: String) {
@@ -57,7 +73,7 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
     fun delete(id: Int) {
         mTaskRepository.delete(id, object : APIListener<Boolean>{
             override fun onSuccess(model: Boolean) {
-                list()
+                list(mTaskFilter)
                 mValidation.value = ValidationListener()
             }
 
